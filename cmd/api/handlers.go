764e022e5a3893181c408cc218170b3c8cb2d586
nsaimk <nsaimkorkmaz1@gmail.com>
2024-05-24 01:02:51 +0100

    returning json from api

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-// Hello is the exported function that handles incoming requests
+// Home is the exported function that handles incoming requests
 // and it will be used as a router parameter in http.HandleFunc
 // ***
 // by adding app *application, same as routes func
-// hello func has access to all the info stored in application
+// Home func has access to all the info stored in application
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello go server! %s", app.Domain)
+	var payload = struct {
+		Status string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status: "active",
+		Message: "Learn with NS Tutorial",
+		Version: "0.1.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
